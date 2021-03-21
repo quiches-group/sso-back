27b965d9d5a9541ca2ef1c503a90356c138a7fc3
maxencemottard <mottard.maxence@gmail.com>
2021-03-21 09:38:47 +0100

    add dto validator behaviour

--- src/epics/user-authentication/dto/loginDTO.ts
+++ src/epics/user-authentication/dto/loginDTO.ts
@@ -1,4 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
 export class LoginDTO {
+  @ApiProperty()
+  @IsNotEmpty()
   mail: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
   password: string;
 }
