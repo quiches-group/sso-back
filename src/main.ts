8cf1c979ad0dbef3da192cc05880640178cfe414
maxencemottard <mottard.maxence@gmail.com>
2021-03-21 09:35:35 +0100

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import config from './config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './epics/app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+
+const useSwagger = (app: INestApplication) => {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Quiches SSO Documentation')
+    .setDescription('The Quiches SSO offcial API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('docs', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  useSwagger(app);
   await app.listen(config().port);
 }
 bootstrap();
