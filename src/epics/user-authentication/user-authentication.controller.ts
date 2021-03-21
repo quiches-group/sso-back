195c7f99df93bb3a1a4f5fc59898db65aafa676a
maxencemottard <mottard.maxence@gmail.com>
2021-03-21 09:36:27 +0100

    remove "/api" prefix

--- src/epics/user-authentication/user-authentication.controller.ts
+++ src/epics/user-authentication/user-authentication.controller.ts
@@ -2,13 +2,13 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { UserAuthenticationService } from './user-authentication.service';
 import { LoginDTO } from './dto/loginDTO';
 
-@Controller('/api')
+@Controller()
 export class UserAuthenticationController {
   constructor(
     private readonly userAuthenticationService: UserAuthenticationService,
   ) {}
 
-  @Post('/login')
+  @Post('login')
   async loginUser(@Body() params: LoginDTO) {
     return await this.userAuthenticationService.loginUser(params);
   }
