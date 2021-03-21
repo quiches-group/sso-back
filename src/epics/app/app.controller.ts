d7c4e1dc23bbf3d21078435c29636b6c19aa00dc
maxencemottard <mottard.maxence@gmail.com>
2021-03-21 09:36:03 +0100

    add status endpoint

---
+++ src/epics/app/app.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+import {
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+
+@ApiTags('Application')
+@Controller()
+export class AppController {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  @ApiOkResponse()
+  @ApiInternalServerErrorResponse()
+  @ApiOperation({ summary: 'Get Application status. Used for monitoring' })
+  @Get('api/status')
+  getApplicationStatus() {
+    const mongooseStatus = this.connection.readyState;
+
+    const data = {
+      'database-status': mongooseStatus === 1 ? 'up' : 'down',
+    };
+
+    if (mongooseStatus !== 1) {
+      throw new HttpException(
+        { ...data, statusCode: HttpStatus.INTERNAL_SERVER_ERROR },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    return data;
+  }
+}
---
+++ src/epics/app/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from '../../config';
+import { User, UserSchema } from '../../models/user.model';
+import { Application, ApplicationSchema } from '../../models/application.model';
+import {
+  ApplicationUser,
+  ApplicationUserSchema,
+} from '../../models/applicationUser.model';
+import {
+  RefreshToken,
+  RefreshTokenSchema,
+} from '../../models/refreshToken.model';
+import { UserRepository } from '../../repositories/user.repository';
+import { ApplicationRepository } from '../../repositories/application.repository';
+import { ApplicationUserRepository } from '../../repositories/applicationUser.repository';
+import { RefreshTokenRepository } from '../../repositories/refreshToken.repository';
+import { UserAuthenticationModule } from '../user-authentication/user-authentication.module';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(config().mongoUrl),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Application.name, schema: ApplicationSchema },
+      { name: ApplicationUser.name, schema: ApplicationUserSchema },
+      { name: RefreshToken.name, schema: RefreshTokenSchema },
+    ]),
+    UserAuthenticationModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    UserRepository,
+    ApplicationRepository,
+    ApplicationUserRepository,
+    RefreshTokenRepository,
+  ],
+})
+export class AppModule {}
