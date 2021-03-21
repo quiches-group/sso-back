936c0f998eaf67b90564ac6ca693d560ef17a911
maxencemottard <mottard.maxence@gmail.com>
2021-03-21 09:41:16 +0100

    Add documentation for existing endpoints

--- src/epics/user-authentication/user-authentication.controller.ts
+++ src/epics/user-authentication/user-authentication.controller.ts
@@ -1,13 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserAuthenticationService } from './user-authentication.service';
 import { LoginDTO } from './dto/loginDTO';
+import {
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('User Authentication')
 @Controller()
 export class UserAuthenticationController {
   constructor(
     private readonly userAuthenticationService: UserAuthenticationService,
   ) {}
 
+  @ApiOkResponse()
+  @ApiUnauthorizedResponse()
   @Post('login')
   async loginUser(@Body() params: LoginDTO) {
     return await this.userAuthenticationService.loginUser(params);
